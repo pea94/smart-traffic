20b6ad5a96377ddc4cfb7e702255bc4cec85cfd1
Pea94 <peangesheya@yahoo.com>
2018-05-07 23:02:05 +0800

    added code to create the list that store the number of cars and the average speeds. Each element is a camera.

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
+from random import randint
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+# 
+    # the colors that are displayed as an ovelay (Baidu JS API object) 
     colors = ['#F14722', '#E67E22', '#F4D03F', '#27AE60']
-    n_car = [100, 90, 80, 50]
-    avg_speed = [40, 50, 60, 70]
+
+    # There are over 20 data collection points we to collect the number of cars passing through each point and the average speed.
+
+    #create list n_car to save the number of cars passing through each intersection.     
+    n_car=[]
+    for i in range(20):
+    n_car.append(randint(0,150))
+
+    #create array avg_speed save the average speed of cars passing through each intersection. 
+    
+    avg_speed=[]
+    for i in range(20):
+        avg_speed.append(randint(10,60))
+
     return render_template('index.html', colors=colors, n_car=n_car, avg_speed=avg_speed)
 
 if __name__ == '__main__':
